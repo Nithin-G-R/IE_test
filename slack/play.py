cbbc6dc44fcd19f79bf57de134bde960c0ef9529
Nithin-G-R <127166295+Nithin-G-R@users.noreply.github.com>
2023-04-06 00:25:26 +1000

    Update play.py

--- slack/play.py
+++ slack/play.py
@@ -18,9 +18,18 @@ def index():
 @play_bp.route("/music", methods=['GET'])
 def music():
     files = [
-        {'name': 'Night Piano', 'url': url_for('static', filename='mp3/night_piano.mp3'), 'img': url_for('static', filename='img/piano.png')},
-        {'name': 'Forest', 'url': url_for('static', filename='mp3/bird.wav'), 'img': url_for('static', filename='img/farm.png')},
-        {'name': 'rainfall', 'url': url_for('static', filename='mp3/rainfall.wav'), 'img': url_for('static', filename='img/rainfall.jpg')},
+        {'name': 'Night Piano', 'url': url_for('static', filename='mp3/night_piano.mp3'), 'img': url_for('static', filename='img/piano_icon.jpg')},
+        {'name': 'Forest', 'url': url_for('static', filename='mp3/bird.wav'), 'img': url_for('static', filename='img/forest_icon.jpg')},
+        {'name': 'Rainfall', 'url': url_for('static', filename='mp3/rainfall.wav'), 'img': url_for('static', filename='img/rainfall_icon.jpg')},
+        {'name': 'Calm down', 'url': url_for('static', filename='mp3/calm_down.mp3'),'img': url_for('static', filename='img/calm_down_icon.jpg')},
 
     ]
     return render_template("play/music.html", files=files)
+
+@play_bp.route("/game", methods=['GET'])
+def game():
+    return render_template("play/game.html")
+
+@play_bp.route("/podcast", methods=['GET'])
+def podcast():
+    return render_template("play/podcast.html")

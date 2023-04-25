08bf9eef426188fc9c7cb3d8276ed9cbf49dfb6f
Zhixuan Li <zlii0308@student.monash.edu>
2023-04-25 17:55:47 +1000

    add chat

--- slack/learn.py
+++ slack/learn.py
@@ -1,9 +1,10 @@
 import functools
 import tempfile
 import time
+import json
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app, jsonify
 )
 from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -24,8 +25,27 @@ def index():
         filename = "{}_{}".format(time.time(), f.filename)
         out_file = os.path.join(current_app.static_folder, filename)
         session['pdf'] = filename
+        #session['pdf'] = "1682300853936868000_LLaMA- Open and Efficient Foundation Language Models.pdf"
         # print(out_file)
         f.save(out_file)
         return render_template("learn/index.html")
     else:
         return render_template("learn/index.html")
+
+
+@learn_bp.route("/chat", methods=["GET"])
+def chat():
+    filename = session['pdf']
+    #filename = "1682300853936868000_LLaMA- Open and Efficient Foundation Language Models.pdf"
+    return render_template("learn/chat.html")
+
+
+@learn_bp.route("/ask", methods=["POST"])
+def ask():
+    body = request.get_json();
+    
+    resp = {
+        "answer": "I don't know your question about {}".format(body.get("question"))
+    }
+    return jsonify(resp)
+    

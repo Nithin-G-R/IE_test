f779090b27722fa3e3a13c6f0a6eefff70002ca8
nithin <ngan0010@student.monash.edu>
2023-04-30 10:17:28 +1000

    Summarize model - LED 648 mb

--- slack/learn.py
+++ slack/learn.py
@@ -4,9 +4,12 @@ import time
 import json
 import PyPDF2
 import requests
-import tensorflow as tf
-from transformers import AutoTokenizer, TFMobileBertForQuestionAnswering
-
+# import tensorflow as tf
+import torch
+# from transformers import AutoTokenizer, TFMobileBertForQuestionAnswering
+from transformers import pipeline
+from azure.storage.blob import BlobServiceClient
+import pickle
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app, jsonify
@@ -20,12 +23,26 @@ from slack.db import get_db
 learn_bp = Blueprint('learn', __name__, url_prefix='/learn')
 
 global current_page, texts
+global model
 current_page = 1
 texts = []
 
 # Load the tokenizer and model from the saved directory
-tokenizer = AutoTokenizer.from_pretrained("models/tfmobilebert")
-model = TFMobileBertForQuestionAnswering.from_pretrained("models/tfmobilebert", from_pt = False)
+# tokenizer = AutoTokenizer.from_pretrained("models/tfmobilebert")
+# model = TFMobileBertForQuestionAnswering.from_pretrained("models/tfmobilebert", from_pt = False)
+
+connect_str = 'DefaultEndpointsProtocol=https;AccountName=summaryhelpviz;AccountKey=2FhNMz4CyiE5AsvGrkTnTUthN9Kskh3HAaaI8t8LA+3S3nzm9vVUXRF3rW8Vss2WPlofgIbEVg1X+AStYO293w==;EndpointSuffix=core.windows.net'
+
+# Define a function to load the model from Blob Storage
+def load_model():
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    blob_client = blob_service_client.get_blob_client(container="summary", blob="led_sum.pickle")
+    model_bytes = blob_client.download_blob().readall()
+    # Load the model into memory here, e.g. using pickle or another library
+    model = pickle.loads(model_bytes)
+    return model
+
+model = load_model()
 
 @learn_bp.route("/", methods=['GET', 'POST'])
 def index():
@@ -75,16 +92,32 @@ def pdf_page():
     return render_template('learn/chat.html', page=current_page, texts = texts, max_length = len(texts))
 
 
-def get_answer(question, context):
-    inputs = tokenizer(question, context, max_length=512, truncation=True, padding='max_length', return_tensors="tf")
-    outputs = model(**inputs)
+# def get_answer(question, context):
+#     inputs = tokenizer(question, context, max_length=512, truncation=True, padding='max_length', return_tensors="tf")
+#     outputs = model(**inputs)
+#
+#     answer_start_index = int(tf.math.argmax(outputs.start_logits, axis=-1)[0])
+#     answer_end_index = int(tf.math.argmax(outputs.end_logits, axis=-1)[0])
+#
+#     predict_answer_tokens = inputs.input_ids[0, answer_start_index: answer_end_index + 1]
+#     answer = tokenizer.decode(predict_answer_tokens)
+#
+#     return answer
 
-    answer_start_index = int(tf.math.argmax(outputs.start_logits, axis=-1)[0])
-    answer_end_index = int(tf.math.argmax(outputs.end_logits, axis=-1)[0])
+def get_answer(context):
 
-    predict_answer_tokens = inputs.input_ids[0, answer_start_index: answer_end_index + 1]
-    answer = tokenizer.decode(predict_answer_tokens)
-
+    result = model(
+        context,
+        min_length=8,
+        max_length=256,
+        no_repeat_ngram_size=3,
+        encoder_no_repeat_ngram_size=3,
+        repetition_penalty=3.5,
+        num_beams=4,
+        do_sample=False,
+        early_stopping=True,
+    )
+    answer = result[0]['summary_text']
     return answer
 
 
@@ -94,7 +127,7 @@ def ask():
     body = request.get_json();
     question = body.get('question')
     context = texts[current_page - 1]
-    answer = get_answer(question = question, context = context)
+    answer = get_answer(context = context)
     resp = {
         "answer": f"{answer}"
     }
